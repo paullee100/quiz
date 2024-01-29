a8d66f589ab2d91b7c4abc78418edefed0a63e9f
paullee100 <leepaul863@gmail.com>
2024-01-29 12:28:10 -0800

    Added short answer questions

--- script.js
+++ script.js
@@ -19,27 +19,60 @@ function startQuiz() {
     showQuestion();
 }
 
+// Create the question one at a time
 function showQuestion() {
     resetState();
+    // A way to display the current question that the user is in
+    // Displays the total number of questions to the user.
     currentNum.innerHTML = `${currentQuestionNum}/${questions.length}`;
+    // Gets the current question from the array.
     let currentQuestion = questions[currentQuestionIndex];
     let questionNo = currentQuestionIndex + 1;
-    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
-    shuffle(currentQuestion.answers);
+    // Get the type of the question
+    let questionType = questions[currentQuestionIndex].type;
 
-    currentQuestion.answers.forEach(answer => {
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+    if (questionType === "multipleChoice") {
+        // If the question type is multiple choice
+        // Shuffle the answers of the current question
+        shuffle(currentQuestion.answers);
+
+        // Create buttons equal to the number of available answers to the question
+        // Can be more than 4 or less
+        // Insert the multiple choice answers based on the index of the questions array
+        currentQuestion.answers.forEach(answer => {
+            const button = document.createElement("button");
+            button.innerHTML = answer.text;
+            button.classList.add("btn");
+            answerButtons.appendChild(button);
+            if (answer.correct) {
+                button.dataset.correct = answer.correct;
+            }
+            button.addEventListener("click", selectAnswer);
+        });
+    } else {
+        const input = document.createElement("input");
+        input.classList.add("inp");
         const button = document.createElement("button");
-        button.innerHTML = answer.text;
+        button.innerHTML = "Confirm";
         button.classList.add("btn");
-        answerButtons.appendChild(button);
-        if (answer.correct) {
-            button.dataset.correct = answer.correct;
-        }
-        button.addEventListener("click", selectAnswer);
-    });
+        answerButtons.append(input);
+        answerButtons.append(button);
+        button.dataset.answerlist = currentQuestion.answers;
+        input.addEventListener("input", function(e) {
+            if (input.value.length == 0) {
+                button.disabled = true;
+            } else {
+                button.disabled = false;
+            }
+        });
+        button.addEventListener("click", (e) => checkAnswer(e, input));
+    }
+    // Add the explanation to the answer to the current question.
     explanation.innerHTML = currentQuestion.explanation;
 }
 
+// Reset the current state to allow the content to be filled with the next question
 function resetState() {
     nextButton.style.display = "none";
     explanation.style.display = "none";
@@ -48,25 +81,68 @@ function resetState() {
     }
 }
 
+// When selecting an answer
 function selectAnswer(e) {
     const selectedBtn = e.target;
+    // Checks if the selected answer is correct.
     const isCorrect = selectedBtn.dataset.correct === "true";
+    // When the user clicks on one of the answer
+    // Check if that answer is correct
     if (isCorrect) {
         selectedBtn.classList.add("correct");
         score++;
     } else {
         selectedBtn.classList.add("incorrect");
     }
+    // This is to ensure that the correct answer is green and if the user
+    // selected a wrong answer, then the current selection will display red
+    // Disable the button so the user would not be able to select another answer
     Array.from(answerButtons.children).forEach(button => {
         if (button.dataset.correct === "true") {
             button.classList.add("correct");
         }
         button.disabled = true;
     });
+    // Display the explanation of the answer on the current question.
     explanation.style.display = "block";
+    // Display the next button to show the next question or show the final result.
     nextButton.style.display = "block";
 }
 
+function checkAnswer(e, textBox) {
+    const confirmBtn = e.target;
+    const answers = confirmBtn.dataset.answerlist.split(",");
+    const total = answers.length;
+    let correctTerms = 0;
+
+    const textContent = textBox.value.split(' ');
+    textContent.forEach(text => {
+        if (answers.includes(text)) {
+            correctTerms++;
+            const index = answers.indexOf(text);
+            if (index > -1) answers.splice(index, 1);
+        }
+    });
+    if (correctTerms/total >= 0.8) {
+        textBox.classList.add("correct");
+
+    } else {
+        textBox.classList.add("incorrect");
+    }
+
+    // This is to ensure that the correct answer is green and if the user
+    // selected a wrong answer, then the current selection will display red
+    // Disable the button so the user would not be able to select another answer
+    Array.from(answerButtons.children).forEach(button => {
+        button.disabled = true;
+    });
+    // Display the explanation of the answer on the current question.
+    explanation.style.display = "block";
+    // Display the next button to show the next question or show the final result.
+    nextButton.style.display = "block";
+}
+
+// Final result to display the score that the user got after the quiz ended.
 function showScore() {
     resetState();
     questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`
@@ -75,8 +151,12 @@ function showScore() {
 }
 
 function handleNextButton() {
+    // Increment counter to get the next question in the array.
     currentQuestionIndex++;
+    // Increment counter to get the current question number.
     currentQuestionNum++;
+    // Check whether or not there are any more questions left
+    // If not, show the final score.
     if (currentQuestionIndex < questions.length) {
         showQuestion();
     } else {
@@ -84,6 +164,7 @@ function handleNextButton() {
     }
 }
 
+// Handles the button to display the next button.
 nextButton.addEventListener("click", () => {
     if (currentQuestionIndex < questions.length) {
         handleNextButton();
@@ -113,4 +194,5 @@ function shuffle(array) {
     return array;
 }
 
+// Driver Function
 startQuiz();
--- variable.js
+++ variable.js
@@ -2,8 +2,54 @@ const square_root = '\u221A';
 const cube_root = '\u221B';
 const fourth_root = '\u221C';
 
+// Absolute Value Problems
+const absoluteVal = [];
+for (let i = 0; i < 2; i++) {
+    let randomChance = Math.random() < 0.5 ? -1 : 1;
+    absoluteVal.push(Math.floor(Math.random() * 1000 * randomChance))
+}
+
+let total = 0;
+// Square Root Problems
+const squareRoot = [];
+for (let i = 0; i < 1; i++) {
+    let randomNum = Math.floor(Math.random() * 100);
+    for (let j = 0; j < randomNum; j++) {
+        total += 2*j+1;
+    }
+    squareRoot.push(total);
+    total = 0;
+}
+total = 1
+// Cube Root Problems
+const cubeRoot = [];
+for (let i = 0; i < 1; i++) {
+    let randomNum = Math.floor((Math.random() + 1) * 10);
+    for (let j = 0; j < 3; j++) {
+        total *= randomNum;
+    }
+    cubeRoot.push(total);
+    total = 1;
+}
+
+// Fourth Root Problems
+const fourthRoot = [];
+for (let i = 0; i < 1; i++) {
+    let randomNum = Math.floor(Math.random() * 10);
+    for (let j = 0; j < 4; j++) {
+        total *= randomNum;
+    }
+    fourthRoot.push(total);
+    total = 1;
+}
+
+// console.log(absoluteVal);
+// console.log(squareRoot);
+// console.log(cubeRoot);
+
 export const questions = [
     {
+        type: "multipleChoice",
         question: "What is an integer?",
         answers: [
             { text: "Any positive or negative whole number, including zero. Integers do not include fractions (1/3), decimals (0.56), or mixed numbers (7*(3/4))", correct: true},
@@ -15,6 +61,7 @@ export const questions = [
         explanation: "Integers are whole numbers including zero."
     },
     {
+        type: "multipleChoice",
         question: "Which value is a prime number?",
         answers: [
             { text: "11", correct: true},
@@ -25,6 +72,7 @@ export const questions = [
         explanation: "A prime number is a number that can only be multiplied by itself, i.e., 11*1."
     },
     {
+        type: "multipleChoice",
         question: "A perfect square is ",
         answers: [
             { text: "another way of writing a fractional exponent.", correct: false},
@@ -35,56 +83,51 @@ export const questions = [
         explanation: "Result of the square root will equal to an integer value."
     },
     {
-        question: "What is the value of " + square_root + "121?",
+        type: "multipleChoice",
+        question: "What is the value of " + square_root + "" + squareRoot[0] + "?",
         answers: [
-            { text: "11", correct: true},
-            { text: "12", correct: false},
-            { text: "13", correct: false},
-            { text: "20", correct: false},
+            { text: "" + Math.sqrt(squareRoot[0]), correct: true},
+            { text: "" + Math.sqrt(squareRoot[0]) / 2, correct: false},
+            { text: "" + Math.sqrt(squareRoot[0]) + (squareRoot[0] % 100), correct: false},
+            { text: "" + squareRoot[0], correct: false},
         ],
-        explanation: "11 * 11 = 121"
+        explanation: Math.sqrt(squareRoot[0]) + " * " + Math.sqrt(squareRoot[0]) + " = " + squareRoot[0]
     },
     {
-        question: "What is the value of " + cube_root + "27?",
+        type: "multipleChoice",
+        question: "What is the value of " + cube_root + "" + cubeRoot[0] + "?",
         answers: [
-            { text: "1", correct: false},
-            { text: "2", correct: false},
-            { text: "3", correct: true},
-            { text: "4", correct: false},
+            { text: "21", correct: false},
+            { text: "" + Math.sqrt(cubeRoot[0]), correct: false},
+            { text: "" + Math.cbrt(cubeRoot[0]), correct: true},
+            { text: "40", correct: false},
         ],
-        explanation: "3 * 3 * 3 = 9 * 3 = 27"
+        explanation: Math.cbrt(cubeRoot[0]) + " * " + Math.cbrt(cubeRoot[0]) + " * " + Math.cbrt(cubeRoot[0]) + " = " + (Math.cbrt(cubeRoot[0])*Math.cbrt(cubeRoot[0])) + " * " + Math.cbrt(cubeRoot[0]) + " = " + cubeRoot[0]
     },
     {
-        question: "How much paper(s) can a printer print out per minute if it takes 30 minutes to print out 25 papers?",
+        type: "multipleChoice",
+        question: "What does |" + absoluteVal[0] + "| = ?",
         answers: [
-            { text: "0.63 paper per minute", correct: false},
-            { text: "1.83 paper per minute", correct: false},
-            { text: "0.83 paper per minute", correct: true},
-            { text: "0.33 paper per minute", correct: false},
-        ],
-        explanation: "25/30 = 0.833333 or 0.83"
-    },
-    {
-        question: "What does |4| = ?",
-        answers: [
-            { text: "-4", correct: false},
+            { text: "" + Math.abs(absoluteVal[0]), correct: true},
             { text: "0", correct: false},
-            { text: "4", correct: true},
+            { text: "" + -Math.abs(absoluteVal[0]), correct: false},
             { text: "None of the above", correct: false}
         ],
         explanation: "Absolute value is the distance from zero, so it will always be a positive number."
     },
     {
-        question: "What does |-1000| = ?",
+        type: "multipleChoice",
+        question: "What does |" + absoluteVal[1] + "| = ?",
         answers: [
-            { text: "-1000", correct: false},
-            { text: "1000", correct: true},
+            { text: "" + -Math.abs(absoluteVal[1]), correct: false},
+            { text: "" + Math.abs(absoluteVal[1]), correct: true},
             { text: "0", correct: false},
             { text: "None of the above", correct: false}
         ],
         explanation: "Absolute value is the distance from zero, so it will always be a positive number."
     },
     {
+        type: "multipleChoice",
         question: "Which of the following values is a perfect square?",
         answers: [
             { text: "12", correct: false},
@@ -95,6 +138,7 @@ export const questions = [
         explanation: square_root + "25 = 5, which is an integer"
     },
     {
+        type: "multipleChoice",
         question: "Which of the following value is equivalent to 4^10?",
         answers: [
             { text: "4*4*4*4*4*4*4*4*4*4", correct: true},
@@ -102,9 +146,10 @@ export const questions = [
             { text: "14", correct: false},
             { text: "4*10", correct: false}
         ],
-        explanation: "Refer to Paul"
+        explanation: "Exponents is the number of the base number times the exponent amount of times"
     },
     {
+        type: "multipleChoice",
         question: "What is the definition of a prime number?",
         answers: [
             { text: "A number that has an integer for its square root. There are 10 perfect squares from 1 to 100.", correct: false},
@@ -112,9 +157,10 @@ export const questions = [
             { text: "Any integer that cannot be divided evenly by 2.", correct: false},
             { text: "Any whole number greater than 1 that has only two factors, itself and 1; that is, a number that can be divided evenly only by 1 and itself.", correct: true}
         ],
-        explanation: "Refer to Paul"
+        explanation: "Definition is the green answer"
     },
     {
+        type: "multipleChoice",
         question: "What is the definition of a composite number?",
         answers: [
             { text: "A superscript number placed next to another number at the top right.", correct: false},
@@ -122,9 +168,10 @@ export const questions = [
             { text: "Cannot be written as fractions or decimals because the number of decimal places is infinite and there is no recurring pattern of digits within the number.", correct: false},
             { text: "Any whole number greater than 1 that has more than two different factors; in other words, any whole number that is not a prime number", correct: true}
         ],
-        explanation: "Refer to Paul"
+        explanation: "Defintion is the green answer"
     },
     {
+        type: "multipleChoice",
         question: "What is the definition of a even number?",
         answers: [
             { text: "Cannot be written as fractions or decimals because the number of decimal places is infinite and there is no recurring pattern of digits within the number.", correct: false},
@@ -132,9 +179,10 @@ export const questions = [
             { text: "Any interger that cannot be divided evenly by 2", correct: false},
             { text: "A superscript number placed next to another number at the top right.", correct: false}
         ],
-        explanation: "Refer to Paul"
+        explanation: "Definition is the green answer"
     },
     {
+        type: "multipleChoice",
         question: "Rational numbers ",
         answers: [
             { text: "is a superscript number placed next to another number at the top right.", correct: false},
@@ -142,9 +190,10 @@ export const questions = [
             { text: "include all integers, decimals, and fractions. Any terminating or repeating decimal number is a rational number.", correct: true},
             { texT: "cannot be written as fractions or decimals because the number of a decimal places is infinite and there is no recurring pattern or digits within the number.", correct: false}
         ],
-        explanation: "Refer to Paul"
+        explanation: "Definition is the green answer"
     },
     {
+        type: "multipleChoice",
         question: "Irrational numbers ",
         answers: [
             { text: "cannot be written as fractions or decimals because the number of decimal places is infinite and there is no recurring pattern of digits within the number.", correct: true},
@@ -152,9 +201,10 @@ export const questions = [
             { text: "are the basic building blocks of mathematics.", correct: false},
             { text: "are any number that uses a decimal point to show the part of the number that is less than one.", correct: false}
         ],
-        explanation: "Refer to Paul"
+        explanation: "Definition is the green answer"
     },
     {
+        type: "multipleChoice",
         question: "Which of the following is NOT an operation",
         answers: [
             { text: "Addition", correct: false},
@@ -166,18 +216,27 @@ export const questions = [
             { text: "Parentheses", correct: false},
             { text: "None of the above", correct: true}
         ],
-        explanation: "Refer to Paul"
+        explanation: "Remember PEMDAS (Parentheses, Exponent, Multiplication, Division, Addition, and Subtraction) and Root is the inverse of Exponent"
     },
     {
+        type: "multipleChoice",
         question: "Which of the following is equivalent to 5*3?",
         answers: [
-            { text: "5+5+5", correct: false},
-            { text: "3+3+3+3+3", correct: false},
+            { text: "5+5+5", correct: true},
+            { text: "3+3+3+3+3", correct: true},
             { text: "5*5*5", correct: false},
             { text: "3*3*3*3*3", correct: false},
-            { text: "All of the above", correct: true},
+            { text: "All of the above", correct: false},
             { text: "None of the above", correct: false}
         ],
-        explanation: "Refer to Paul"
+        explanation: "Add them up. 5*3 is 15. 5+5+5=10+5=15\n3+3+3+3+3=6+3+3+3=9+3+3=12+3=15"
     },
+    {
+        type: "shortAnswer",
+        question: "What is 9 + 10 * 2",
+        answers: [
+            "29",
+        ],
+        explanation: "Using PEMDAS, multiplication would come first, then addition\n 9 + 10 * 2 = 9 + 20 = 29"
+    }
 ];
